b698ebce824529927b5dcb04aa552183b46efe6e
Makarenko-Maryna <makmar1517@gmail.com>
2021-11-15 21:39:39 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,21 +1,6 @@
 // Напиши скрипт, который реагирует на изменение значения input#font - size - control(событие input) и изменяет инлайн - стиль span#text обновляя свойство font - size. 
 // В результате при перетаскивании ползунка будет меняться размер текста.
 
-// const button = document.querySelector(".btn");
-
-// button.style.backgroundColor = "teal";
-// button.style.fontSize = "24px";
-// button.style.textAlign = "center";
-
-// console.log(button.style); // inline styles object
-
-// const fontSize = document.querySelector("input#font-size-control");
-// function () {
-
-// }
-
-// fontSize.style.fontSize = min
-// // span#text
 
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('span');
